59fdb0a0768727238d74c5db3acb14ee70ef94dc
mlerena <manuellerena@gmail.com>
2021-01-25 14:33:02 -0300

    Test contentURl

--- src/android/CDVInstagramPlugin.java
+++ src/android/CDVInstagramPlugin.java
@@ -134,8 +134,10 @@ public class CDVInstagramPlugin extends CordovaPlugin {
                         this.cordova.getActivity().getPackageName() + ".provider",
                         file);
 
+
                 shareIntent.putExtra(Intent.EXTRA_STREAM, photoURI);
                 shareIntent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                shareIntent.putExtra("com.instagram.sharedSticker.contentURL", "brisamusic://");
             }
 
             shareIntent.putExtra(Intent.EXTRA_TEXT, captionString);
